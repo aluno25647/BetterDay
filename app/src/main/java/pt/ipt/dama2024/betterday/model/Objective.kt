6690c1f5a1fdc06040a8d02df1a4c6c0803bc7ad
DESKTOP-4F5D5P1\Paulo Peixoto <aluno26097@ipt.pt>
2024-06-06 15:25:06 +0100

    Fixed Objective class

--- app/src/main/java/pt/ipt/dama2024/betterday/model/Objective.kt
+++ app/src/main/java/pt/ipt/dama2024/betterday/model/Objective.kt
@@ -6,9 +6,17 @@ import java.util.Date
 
 @Entity(tableName = "objectives")
 data class Objective(
-    @PrimaryKey(autoGenerate = true) val id: Long = 0,
-    val title: String,
-    val description: String,
-    val frequency: String, // "daily", "weekly", "monthly", "unique"
-    val date: Date? = null //
+    @PrimaryKey(autoGenerate = true) val id: Long = 0, // Unique identifier for the objective
+    val title: String, // Title of the objective
+    val description: String, // Description of the objective
+    val frequency: Frequency, // Frequency of the objective (daily, weekly, etc)
+    val creationDate: Date, // Date when the objective was created
+    val deadline: Date, // Deadline for the objective
+    val checked: Boolean = false, // Whether the objective has been checked/completed
+    val author: String, // User ID or username of the author/creator
 )
+
+// Possible frequencies for the objective
+enum class Frequency {
+    DAILY, WEEKLY, MONTHLY, UNIQUE
+}
--- app/src/main/java/pt/ipt/dama2024/betterday/ui/activity/MainActivity.kt
+++ app/src/main/java/pt/ipt/dama2024/betterday/ui/activity/MainActivity.kt
@@ -1,12 +1,22 @@
-package pt.ipt.dama2024.betterday
+package pt.ipt.dama2024.betterday.ui.activity
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import pt.ipt.dama2024.betterday.MyViewPageAdapter
+import pt.ipt.dama2024.betterday.R
 
 class MainActivity : AppCompatActivity() {
+
+    // Auxiliary vars.
+    lateinit var tabLayout: TabLayout
+    lateinit var viewPager2: ViewPager2
+    private lateinit var viewPagerAdapter: MyViewPageAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,33 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Initiate the auxiliary vars
+        tabLayout = findViewById(R.id.tab_layout)
+        viewPager2 = findViewById(R.id.view_pager2)
+
+        // Initiate the ' fragment adapter'
+        viewPagerAdapter = MyViewPageAdapter(this)
+        // Link the viewPage2 to the Adapter
+        viewPager2.adapter = viewPagerAdapter
+
+        // Find the tab that the user clicked
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(p0: TabLayout.Tab?) {
+                viewPager2.currentItem = p0!!.position
+            }
+            override fun onTabReselected(p0: TabLayout.Tab?) {}
+            override fun onTabUnselected(p0: TabLayout.Tab?) {}
+        })
+
+        // Format the behave of viewpage with the tabLayout
+        viewPager2.registerOnPageChangeCallback(
+            object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    tabLayout.getTabAt(position)?.select()
+                }
+            }
+        )
     }
 }
