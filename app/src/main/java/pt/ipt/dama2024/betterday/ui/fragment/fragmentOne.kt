8ba6eccde6376537d06f7e5c35039e71fbaa488d
Luís Costa <aluno25647@ipt.pt>
2024-06-09 04:04:48 +0100

    setup for the display of fragmentOne (TODO clickbutton)

--- app/src/main/java/pt/ipt/dama2024/betterday/ui/fragment/fragmentOne.kt
+++ app/src/main/java/pt/ipt/dama2024/betterday/ui/fragment/fragmentOne.kt
@@ -4,16 +4,43 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import pt.ipt.dama2024.betterday.R
+import pt.ipt.dama2024.betterday.data.ObjectiveRepository
+import pt.ipt.dama2024.betterday.ui.adapter.ObjectiveAdapter
 
 
 class FragmentOne : Fragment() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: ObjectiveAdapter
+    private lateinit var newObjectiveButton: FloatingActionButton
+    private lateinit var objectiveRepository: ObjectiveRepository
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_one, container, false)
+        val view = inflater.inflate(R.layout.fragment_one, container, false)
+
+        recyclerView = view.findViewById(R.id.recyclerViewObjectives)
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        // TODO username
+        val objectives = objectiveRepository.getAllUserObjectives(username = "")
+
+        adapter = ObjectiveAdapter(objectives)
+        recyclerView.adapter = adapter
+
+        // Initialize FloatingActionButton
+        newObjectiveButton = view.findViewById(R.id.buttonAddObjective)
+        newObjectiveButton.setOnClickListener {
+            // TODO add new objective action
+        }
+
+        return view
     }
 
 }
