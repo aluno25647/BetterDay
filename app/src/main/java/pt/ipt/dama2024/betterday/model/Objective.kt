dcd7178dfe634b6a3cb2a82bf3c8cdd9f08afa71
Luís Costa <aluno25647@ipt.pt>
2024-06-13 17:04:24 +0100

    Objective changes to only receive 1 photo instead of 3

--- app/src/main/java/pt/ipt/dama2024/betterday/model/Objective.kt
+++ app/src/main/java/pt/ipt/dama2024/betterday/model/Objective.kt
@@ -13,8 +13,6 @@ data class Objective(
     var checked: Boolean = false, // Whether the objective has been checked/completed
     val author: String, // User ID or username of the author/creator
     val photo1: ByteArray? = null,  // ByteArray for the first photo
-    val photo2: ByteArray? = null,  // ByteArray for the second photo
-    val photo3: ByteArray? = null,  // ByteArray for the third photo
     val latitude: Double? = null,   // Latitude for GPS coordinates
     val longitude: Double? = null   // Longitude for GPS coordinates
 ) {
@@ -38,14 +36,6 @@ data class Objective(
             if (other.photo1 == null) return false
             if (!photo1.contentEquals(other.photo1)) return false
         } else if (other.photo1 != null) return false
-        if (photo2 != null) {
-            if (other.photo2 == null) return false
-            if (!photo2.contentEquals(other.photo2)) return false
-        } else if (other.photo2 != null) return false
-        if (photo3 != null) {
-            if (other.photo3 == null) return false
-            if (!photo3.contentEquals(other.photo3)) return false
-        } else if (other.photo3 != null) return false
         if (latitude != other.latitude) return false
         if (longitude != other.longitude) return false
 
@@ -60,8 +50,6 @@ data class Objective(
         result = 31 * result + checked.hashCode()
         result = 31 * result + author.hashCode()
         result = 31 * result + (photo1?.contentHashCode() ?: 0)
-        result = 31 * result + (photo2?.contentHashCode() ?: 0)
-        result = 31 * result + (photo3?.contentHashCode() ?: 0)
         result = 31 * result + (latitude?.hashCode() ?: 0)
         result = 31 * result + (longitude?.hashCode() ?: 0)
         return result
