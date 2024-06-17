4ebcbbcdbd07014db8ba9b8aa08626324943177a
Luís Costa <aluno25647@ipt.pt>
2024-06-17 14:22:10 +0100

    upgrade version for store

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "pt.ipt.dama2024.betterday"
         minSdk = 24
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "1.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
